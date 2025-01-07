2ffe3cbc3e346320d03f04aab5c9c47f17c389ba
Jaiden Ratti <jaidenratti@gmail.com>
2025-01-07 01:00:11 -0500

    usual

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Poppins, Roboto } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
+
 
 const poppins = Poppins({ 
   weight: ['400', '700'],
@@ -25,7 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.variable} ${roboto.variable}`}>
-      <body>{children}</body>
+      <body>{children}
+      <Analytics/>
+      </body>
     </html>
   )
 }
